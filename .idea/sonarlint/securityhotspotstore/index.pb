ebfee6f908e5a6953bee876e6094caa93866f75e
ZAS <777ergoproxy@gmail.com>
2024-04-06 11:19:33 +0300

    Slf4J with Lombok\error providers

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+j
+:Maxima/src/main/java/lesson03_22/lesson03_19/Solution.java,d\4\d4248bfcd9ddb65d567c561feadb8ae5e5386cfd
+p
+@Maxima/src/main/java/lesson03_22/lesson03_19Extend/Solution.java,1\1\11f9aefb0ae17347f82ecd90cf652a62d21cd878
+>
+Maxima/pom.xml,0\5\05101f0da703b54293515bf6610ca033a0ac71c4
---
+++ Maxima/src/main/java/lesson03_22/lesson03_19Extend/Solution.java
@@ -0,0 +1,45 @@
+package lesson03_22.lesson03_19Extend;
+
+import lombok.extern.slf4j.Slf4j;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("a");
+
+        MyThread firstThread = new MyThread(sb);
+        MyThread secondThread = new MyThread(sb);
+        MyThread thirdThread = new MyThread(sb);
+
+
+        firstThread.start();
+        secondThread.start();
+        thirdThread.start();
+
+
+    }
+}
+
+@Slf4j
+class MyThread extends Thread {
+
+    private StringBuilder sb;
+
+    public MyThread(StringBuilder sb) {
+        this.sb = sb;
+    }
+
+    @Override
+    public void run() {
+        synchronized (sb) {
+            for (int i = 0; i < 100; i++) {
+//                System.out.println("It is sout from Thread: " + sb);
+                log.info("It is sout from Thread: " + sb);
+            }
+            sb.setCharAt(0, (char) (sb.charAt(0) + 1));
+        }
+    }
+}
+ 
